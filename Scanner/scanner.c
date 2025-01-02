08b5fb08309db5fe0c809b1978618fa0bf8ba036
SCanakis <sac2693@g.rit.edu>
2025-01-02 01:08:09 +0000

    working on getNextToken created helper functions on the way

--- Scanner/scanner.c
+++ Scanner/scanner.c
@@ -13,9 +13,114 @@ static int column = 0; // Current column number
 static int length = 0; // Current column number
 
 
+void skipToNextLine() {
 
+	while(source[pos] != '\n') {
+		pos++;
+	}
+
+	pos++;
+	line++;
+	col = 0;
+}
+
+
+char getNextChar() {
+	char currChar = source[pos];
+	pos++;
+	column++;
+
+	// Checks for whitespace or next line
+	while(isspace(currChar) || currChar == '\n || currChar == '/' ) {
+		
+		// Handles Comments
+		if(currChar == '/' && source[pos] == '/') {
+			skipToNextLine()
+		}
+		
+
+		// Handles skipping lines and blank space
+		if(currChar == '\n') {
+			skipToNextLine();
+
+		} else if(isspace(currChar)) {
+			pos++;
+			col++;
+		}
+		currChar = source[pos];
+	}
+	return currChar;
+}
+
+// Skips to next line without affecting col or line
+int skipFakeLine(int tempPos) {
+
+	while(source[tempPos] != '\n') {
+		tempPos++;
+	}
+
+	return tempPos++;
+}
+
+char peekNextChar() {
+	int tempPos = pos;
+
+	char currChar = source[tempPos];
+	tempPos++;
+
+	// Checks for whitespace or next line
+	while(isspace(currChar) || currChar == '\n || currChar == '/' ) {
+		
+		// Handles Comments
+		if(currChar == '/' && source[pos] == '/') {
+			tempPos = skipFakeLine(tempPos);
+		}
+		
+		// Handles skipping lines and blank space
+		if(currChar == '\n') {
+			skipToNextLine();
+
+		} else if(isspace(currChar)) {
+			tempPos++;
+		}
+
+		currChar = source[tempPos];
+
+	}
+	return currChar;
+
+}
 
 token_t getNextToken() {
+
+	char currChar = getNextChar(); 
+
+	// Checks whether currChar is a digits or currChar is 
+	if(isdigit(currChar)) {
+		int currInt = atoi(&currChar);
+		int totalInt = 0;
+		double totalDouble = 0;
+		double currDouble = 0;
+		while(isdigit(peekNextChar()) || peekNextChar() == '.' ) {
+			
+			if(isdigit(peekNextChar())) {
+				total = total * 10 +  currInt;
+				currInt = atoi(getNextChar());
+			} else {
+				// Handle Deciamls
+			}
+
+		}	
+
+	
+	} else if (isalpha(currChar)) {
+
+	} else {
+		checkForSymbols();
+		checkForOperators();
+	}
+
+
 	return NULL;
 }
 
